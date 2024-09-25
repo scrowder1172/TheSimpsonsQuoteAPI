2bb87b232eb8a1f48078ac59fb1d8088a4b92367
Scott Crowder <scrowder@gmail.com>
2024-09-25 09:06:43 -0500

    Initial Commit

---
+++ TheSimpsonsQuoteAPI/ContentView.swift
@@ -0,0 +1,27 @@
+//
+// File: ContentView.swift
+// Project: TheSimpsonsQuoteAPI
+// 
+// Created by SCOTT CROWDER on 9/25/24.
+// 
+// Copyright © Playful Logic Studios, LLC 2024. All rights reserved.
+// 
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TheSimpsonsQuoteAPI/TheSimpsonsQuoteAPIApp.swift
@@ -0,0 +1,20 @@
+//
+// File: TheSimpsonsQuoteAPIApp.swift
+// Project: TheSimpsonsQuoteAPI
+// 
+// Created by SCOTT CROWDER on 9/25/24.
+// 
+// Copyright © Playful Logic Studios, LLC 2024. All rights reserved.
+// 
+
+
+import SwiftUI
+
+@main
+struct TheSimpsonsQuoteAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
